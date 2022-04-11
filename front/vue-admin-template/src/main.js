50ba2ebd48ef1b2dae23d99901fa82c252ee95d7
WangPengLiang <15101587969@163.com>
2022-04-11 17:17:20 +0800

    update

--- front/vue-admin-template/src/main.js
+++ front/vue-admin-template/src/main.js
@@ -6,7 +6,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
-import '@/styles/index.scss' // global css
+import '@/styles/index.scss' // 导入本地css
 
 import App from './App'
 import store from './store'
--- front/vue-admin-template/src/settings.js
+++ front/vue-admin-template/src/settings.js
@@ -1,16 +1,11 @@
 module.exports = {
 
+  // 项目名称
   title: 'Vue Admin Template',
 
-  /**
-   * @type {boolean} true | false
-   * @description Whether fix the header
-   */
+  // 固定头部
   fixedHeader: false,
 
-  /**
-   * @type {boolean} true | false
-   * @description Whether show the logo in sidebar
-   */
+  // 显示左侧菜单logo
   sidebarLogo: false
 }
