89469b96aa8ee388ee93eee2cfba4e61120fe8a2
WangPengLiang <15101587969@163.com>
2022-04-12 15:46:59 +0800

    update

--- backend/main.go
+++ backend/main.go
@@ -5,46 +5,57 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mssql"
+	"gorm.io/gorm"
+	_ "gorm.io/mssql"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Person struct {
+	Id      int `gorm:"primaryKey"`
 	Name    string
 	Age     int
 	Address string
 }
 
-var (
-	server   = "localhost"
-	port     = 1433
-	user     = "sa"
-	database = "go_db"
-	password = "wpl19950815"
-)
+func (Person) TableName() string {
+	return "Person"
+}
 
 func main() {
-	// 创建一个默认路由
 	r := gin.Default()
-	connStr := fmt.Sprintf("server=%s;port=%d;database=%s;user id=%s;password=%s", server, port, database, user, password)
-	db, err := gorm.Open("mssql", connStr)
-	if err != nil {
-		log.Fatal("连接数据库失败:" + err.Error())
-	}
 
-	db.AutoMigrate(&Person{})
-	db.Create(&Person{Name: "w1", Address: "beijing", Age: 18})
-	db.Create(&Person{Name: "w2", Address: "shanghai", Age: 18})
-	db.Create(&Person{Name: "w3", Address: "shenzhen", Age: 18})
-
-	defer db.Close()
 	r.GET("/", func(c *gin.Context) {
+		db := createDbConn()
 		var person []Person
 		db.Find(&person)
 		c.JSON(http.StatusOK, person)
 	})
-
 	r.Run()
+}
+
+// 返回数据库连接
+func createDbConn() *gorm.DB {
+	var (
+		server   = "localhost"
+		port     = 1433
+		user     = "sa"
+		database = "go_db"
+		password = "wpl19950815"
+	)
+
+	// 数据库连接字符串
+	connString := fmt.Sprintf("server=%s;port=%d;database=%s;user id=%s;password=%s", server, port, database, user, password)
+	db, err := gorm.Open("mssql", connString)
+	if err != nil {
+		log.Fatal("create dbconn failed!:" + err.Error())
+	}
+
+	// 启用自动迁移
+	db.AutoMigrate(&Person{})
+	// 创建2条测试数据
+	db.Create(&Person{Name: "wangpengliang", Address: "beijing", Age: 18})
+	db.Create(&Person{Name: "lizimeng", Address: "shanghai", Age: 18})
+
+	return db
 }
